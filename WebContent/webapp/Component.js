b15aac6f767484456a244e6957a90b9a7d6b7520
yonut <yonutz_qx3@yahoo.com>
2018-04-16 00:04:32 +0300

    Training

---
+++ WebContent/webapp/Component.js
@@ -0,0 +1,35 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/Device"
+], function(UIComponent,JSONModel,Device) {
+	"use strict";
+
+	return UIComponent.extend("sap.suite.ui.Tdc.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+		init: function() {
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+			
+			
+			// set device model
+			var oDeviceModel = new JSONModel(Device);
+			oDeviceModel.setDefaultBindingMode("OneWay");
+			this.setModel(oDeviceModel, "device");
+			
+		},
+		createContent: function() {
+			// create root view
+			return sap.ui.view("AppView", {
+				viewName: "sap.suite.ui.Tdc.view.App",
+				type: "XML"
+			});
+		}
+	});
+});
---
+++ WebContent/webapp/controller/FView.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/format/NumberFormat"
+], function(Controller, MessageToast, JSONModel, NumberFormat) {    
+	"use strict";
+
+	return Controller.extend("sap.suite.ui.Tdc.controller.FView", {
+		onInit: function() {  
+			
+		}
+	});
+});  
